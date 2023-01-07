c1c98844f438b4c1e09d798e764609edbc9c60b4
Stéphane Senart <stephane.senart@gmail.com>
2023-01-07 18:39:21 +0100

    Add offset and phase calibration parameters.

--- include/EnergyMeter.h
+++ include/EnergyMeter.h
@@ -18,17 +18,22 @@ public:
         float samplingFrequency,
         unsigned long samplingDurationInMs,
         float calibrationVoltageMultiplier,
-        float calibrationCurrentMultiplier,
-        float calibrationPhaseShift)
+        float calibrationVoltageOffset,
+        const float calibrationCurrentMultiplier[],
+        const float calibrationCurrentOffset[],
+        const float calibrationPhaseShift[],
+        int currentInputCount)
     : energySource_(energySource),
     samplingFrequency_(samplingFrequency),
     samplingDurationInMs_(samplingDurationInMs),
     voltageMultiplier_(supplyVoltage * calibrationVoltageMultiplier / 2),
-    currentMultiplier_(supplyVoltage * calibrationCurrentMultiplier / 2),
-    calibrationPhaseShift_(calibrationPhaseShift),
+    voltageOffset_(calibrationVoltageOffset),
+    currentMultiplier_(),
+    currentOffset_(),
+    calibrationPhaseShift_(),
+    currentInputCount_(currentInputCount),
     frequency_(0.0),
-    rmsVoltage_(0.0),
-    currentInputCount_(0),
+    rmsVoltage_(0.0),    
     rmsCurrent_(),
     apparentPower_(),
     activePower_(),
@@ -37,6 +42,12 @@ public:
     powerFactor_(),
     phaseAngle_()
     {
+        for (auto inputIndex = 0; inputIndex < currentInputCount_; ++inputIndex)
+        {
+            currentMultiplier_[inputIndex] = supplyVoltage * calibrationCurrentMultiplier[inputIndex] / 2;
+            currentOffset_[inputIndex] = calibrationCurrentOffset[inputIndex];
+        }
+        memcpy(calibrationPhaseShift_, calibrationPhaseShift, currentInputCount_ * sizeof(float));
     }
 
     void update()
@@ -49,13 +60,15 @@ public:
 
         frequency_ = ((float) sampling.periodCount) / samplingDurationInMs_ * MILLISECONDS_PER_SECOND;
         rmsVoltage_ = voltageMultiplier_ * sqrt(sampling.voltageSqrSum / sampling.sampleCount);
+        averageVoltage_ = sampling.voltageSum / sampling.sampleCount * rmsVoltage_ * sqrt(2);
 
-        currentInputCount_ = sampling.currentInputCount;
+        //currentInputCount_ = sampling.currentInputCount;
         for (auto inputIndex = 0; inputIndex < sampling.currentInputCount; ++inputIndex)
-        {
-            rmsCurrent_[inputIndex] = currentMultiplier_ * sqrtf(sampling.currentSqrSum[inputIndex] / sampling.sampleCount);
+        {            
+            rmsCurrent_[inputIndex] = currentMultiplier_[inputIndex] * sqrtf(sampling.currentSqrSum[inputIndex] / sampling.sampleCount);
+            averageCurrent_[inputIndex] = sampling.currentSum[inputIndex] / sampling.sampleCount * rmsCurrent_[inputIndex] * sqrt(2);
             apparentPower_[inputIndex] = rmsVoltage_ * rmsCurrent_[inputIndex];
-            activePower_[inputIndex] = voltageMultiplier_ * currentMultiplier_ * sampling.instantPowerSum[inputIndex] / sampling.sampleCount;
+            activePower_[inputIndex] = voltageMultiplier_ * currentMultiplier_[inputIndex] * sampling.instantPowerSum[inputIndex] / sampling.sampleCount;
             reactivePower_[inputIndex] = sqrtf(max(0.0, sqr(apparentPower_[inputIndex]) - sqr(activePower_[inputIndex])));
             energy_[inputIndex] += activePower_[inputIndex] * (((float) samplingDurationInMs_) / MILLISECONDS_PER_HOUR);
             powerFactor_[inputIndex] = max(-1.0, min(1.0, activePower_[inputIndex] / apparentPower_[inputIndex]));
@@ -72,14 +85,26 @@ public:
     /* Frequency in Hertz (Hz). */ 
     float frequency() { return frequency_; }
 
+    /* Average voltage value in Volt (V). Used for calibration. It should be zero.*/ 
+    float averageVoltage() { return averageVoltage_; }
+
+    /* Average current value in Ampere (A). Used for calibration. It should be zero.*/ 
+    float averageCurrent(int inputIndex) { return averageCurrent_[inputIndex]; }
+
     /* RMS voltage in Volt (V). */
     float rmsVoltage() { return rmsVoltage_; }
+
+    /* Peak voltage in Volt (V). */
+    float peakVoltage() { return rmsVoltage_ * sqrt(2); }
 
     /* Number of current input. */
     int currentInputCount() { return currentInputCount_; }
 
     /* RMS current in Ampere (A). */
     float rmsCurrent(int inputIndex) { return rmsCurrent_[inputIndex]; }
+
+    /* Peak current in Ampere (A). */
+    float peakCurrent(int inputIndex) { return rmsCurrent_[inputIndex] * sqrt(2); }
 
     /* Apparent power in Volt-Ampere (VA). */
     float apparentPower(int inputIndex) { return apparentPower_[inputIndex]; }
@@ -111,6 +136,8 @@ private:
     {
         unsigned long sampleCount;
         unsigned long periodCount;
+        float voltageSum;
+        float currentSum[MAX_CURRENT_INPUT];
         float voltageSqrSum;
 
         int currentInputCount;
@@ -125,11 +152,13 @@ private:
 
         sampling.sampleCount = 0;
         sampling.periodCount = 0;
+        sampling.voltageSum = 0.0;
         sampling.voltageSqrSum = 0.0;
 
         sampling.currentInputCount = energySource_.currentInputCount();
         for (auto index = 0; index < sampling.currentInputCount; ++index)
         {
+            sampling.currentSum[index] = 0.0;
             sampling.currentSqrSum[index] = 0.0;
             sampling.instantPowerSum[index] = 0.0;
         }
@@ -140,6 +169,7 @@ private:
         auto samplingPeriod = ((float) MICROSECONDS_PER_SECOND) / samplingFrequency_;
 
         auto previousVoltage = 0.0;
+        float previousCurrent[MAX_CURRENT_INPUT];
 
         unsigned long previousTime = 0;
 
@@ -159,21 +189,31 @@ private:
 
                 energySource_.capture(energySample);
 
-                auto voltage = energySample.voltage();
+                auto voltage = energySample.voltage() + voltageOffset_;
 
                 if (sampling.sampleCount > 0 && previousVoltage <= 0.0 && voltage > 0.0)
                 {
                     ++sampling.periodCount;
                 }
 
+                sampling.voltageSum += voltage;
                 sampling.voltageSqrSum += voltage * voltage;
 
                 for (auto index = 0; index < sampling.currentInputCount; ++index)
                 {
-                    auto current = energySample.current(index);
+                    auto current = energySample.current(index) + currentOffset_[index];
 
-                    sampling.currentSqrSum[index] += current * current;
-                    sampling.instantPowerSum[index] += voltage * current;
+                    float phaseAdjustedCurrent = current;
+                    if (sampling.sampleCount > 0)
+                    {
+                        phaseAdjustedCurrent = previousCurrent[index] + calibrationPhaseShift_[index] * (current - previousCurrent[index]);
+                    }
+
+                    sampling.currentSum[index] += phaseAdjustedCurrent;
+                    sampling.currentSqrSum[index] += phaseAdjustedCurrent * phaseAdjustedCurrent;
+                    sampling.instantPowerSum[index] += voltage * phaseAdjustedCurrent;
+
+                    previousCurrent[index] = current;
                 }
 
                 previousVoltage = voltage;
@@ -191,15 +231,20 @@ private:
     float samplingFrequency_;
     unsigned long samplingDurationInMs_;
     float voltageMultiplier_;
-    float currentMultiplier_;
-    float calibrationPhaseShift_;
+    float voltageOffset_;
+    float currentMultiplier_[MAX_CURRENT_INPUT];
+    float currentOffset_[MAX_CURRENT_INPUT];
+    float calibrationPhaseShift_[MAX_CURRENT_INPUT];
+    int currentInputCount_;
 
     unsigned long sampleCount_;
 
     float frequency_;
+    float averageVoltage_;
+    float averageCurrent_[MAX_CURRENT_INPUT];
     float rmsVoltage_;
 
-    int currentInputCount_;
+
     float rmsCurrent_[MAX_CURRENT_INPUT];
     float apparentPower_[MAX_CURRENT_INPUT];
     float activePower_[MAX_CURRENT_INPUT];
--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,36 @@
 // See: https://solarduino.com/how-to-measure-ac-power-with-arduino/
 
 // ---------------------------
-// -- Calibration parameters
+// -- Board parameters
 const float SUPPLY_VOLTAGE = 5.0; // Volts.
 const float ADC_RESOLUTION = 10; // Bits
-const float CALIBRATION_VOLTAGE_MULTIPLIER = 130.0; // 130 * 2.5V = 325V (230V RMS)
-const float CALIBRATION_CURRENT_MULTIPLIER = 0.24596; // 0.24596 * 2.5V = 0.6149A (0.4348A RMS) => Equivalent to 100W.
-const float CALIBRATION_PHASE_SHIFT = 1.5; // Coefficient.
+
+// ---------------------------
+// -- Calibration parameters
+
+
+// The voltage multiplier corresponds to the voltage transformer ratio (turn ratio).
+//    Example: We have an input voltage transformer from 230Vrms (650Vpp) to 1.76Vrms (5Vpp).
+//             For a maximum input voltage of 230Vrms, we choose a voltage multiplier:
+//                  CALIBRATION_VOLTAGE_MULTIPLIER = 230 / 1.76 = 650 / 5 = 130.
+const float CALIBRATION_VOLTAGE_MULTIPLIER = 130;
+const float CALIBRATION_VOLTAGE_OFFSET = 0.0;
+
+// The current multiplier corresponds to the current sensor sensitivity (V/A) or the current transformer ratio (turn ratio).
+//    Example 1: We have an input current sensor with a sensitivity of 66mV/A (example: sensor ACS712 - 30A).
+//               For a maximum input current of 0.4348Arms (equivalent to 100W at 230Vrms), we choose a current multiplier:
+//                  CALIBRATION_CURRENT_MULTIPLIER = 0.4348 * 0.066 = 0.0286968.
+//    Example 2: We have an input current transformer with a turn ratio 15A:0.05A (example: YHDC SCT-013-015).
+//               For a maximum input current of 0.4348Arms (equivalent to 100W at 230Vrms), we choose a current multiplier:
+//                  CALIBRATION_CURRENT_MULTIPLIER =
+const float CALIBRATION_CURRENT_MULTIPLIER[] = { 0.24596, 0.24596, 0.24596, 0.24596, 0.24596, 0.24596, 0.24596, 0.24596, 0.24596, 0.24596 }; // 0.24596 * 2.5V = 0.6149A (0.4348A RMS) => Equivalent to 100W.
+const float CALIBRATION_CURRENT_OFFSET[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+// Phase correction coefficient:
+// - 0 equivalent to move the waveform to the left of one sample period.
+// - 1 equivalent to no change, the waveform remains the same.
+// - 2 equivalent to move the waveform to the right of one sample period.
+const float CALIBRATION_PHASE_SHIFTS[] = { 0.885 , 0.77, 0.665, 0.54, 0.425, 0.31, 0.195, 0.08 , 1.0, 1.0 };
 
 // ---------------------------
 // -- Simulation mode parameters
@@ -44,8 +68,11 @@ EnergyMeter energyMeter(energySource,
                         SAMPLING_FREQUENCY,
                         SAMPLING_DURATION_MS,
                         CALIBRATION_VOLTAGE_MULTIPLIER,
+                        CALIBRATION_VOLTAGE_OFFSET,
                         CALIBRATION_CURRENT_MULTIPLIER,
-                        CALIBRATION_PHASE_SHIFT
+                        CALIBRATION_CURRENT_OFFSET,
+                        CALIBRATION_PHASE_SHIFTS,
+                        10
                        );
 // ---------------------------
 
@@ -59,36 +86,50 @@ void loop() {
 
   energyMeter.update();
 
+  Serial.println("------ Voltage ------");
+  Serial.println(" Sp    Hz    Avg    Pk      Rms");
   Serial.print(energyMeter.sampleCount());
-  Serial.print(" ");
+  Serial.print("  ");
   Serial.print(energyMeter.frequency());
-  Serial.print(" ");
+  Serial.print("  ");
+  Serial.print(energyMeter.averageVoltage());  
+  Serial.print("  ");
+  Serial.print(energyMeter.peakVoltage());  
+  Serial.print("  ");  
   Serial.println(energyMeter.rmsVoltage());
 
+  Serial.println("------ Currents ------");
+  Serial.println(" Idx Avg    Pk    Rms   VA      W      VAR     Wh    PF    Phi");
   for (auto inputIndex = 0; inputIndex < energyMeter.currentInputCount(); ++inputIndex)
   {
       Serial.print("- ");
       Serial.print(inputIndex);
-      Serial.print(" ");
+      Serial.print("  ");
+      Serial.print(energyMeter.averageCurrent(inputIndex));      
+      Serial.print("  ");
+      Serial.print(energyMeter.peakCurrent(inputIndex));      
+      Serial.print("  ");      
       Serial.print(energyMeter.rmsCurrent(inputIndex));
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.print(energyMeter.apparentPower(inputIndex));
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.print(energyMeter.activePower(inputIndex));
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.print(energyMeter.reactivePower(inputIndex));
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.print(energyMeter.energy(inputIndex));
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.print(energyMeter.powerFactor(inputIndex));    
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.println(energyMeter.phaseAngle(inputIndex));
   }
 
+  Serial.println("------ Performance ------");
+  Serial.println("TotalTime  BusyTime");
   Serial.print(energyMeter.totalElapsed());
   Serial.print(" ");
   Serial.println(energyMeter.busyElapsed());
-  Serial.println("-------------");  
+  Serial.println("---------------------------------------");  
 }
 
 #ifndef ARDUINO
